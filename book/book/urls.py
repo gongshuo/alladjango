6a5251b77bc4b4aa3351156a6c5f155496627d48
gongshuo <470690988@qq.com>
2020-08-07 11:10:59 +0800

    提交

--- book/book/urls.py
+++ book/book/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-# from users.views import BookAPIView1
+from users.views import BookAPIView1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('apibook1/',BookAPIView1.as_view(),name='book1'),
+    path('apibook1/', BookAPIView1.as_view(), name='book1'),
 ]
