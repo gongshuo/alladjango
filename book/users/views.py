2ac6b9e7cb79d2e15b73c75dd550040f1d16c92f
gongshuo <470690988@qq.com>
2020-08-08 23:52:17 +0800

    提交

--- book/users/views.py
+++ book/users/views.py
@@ -12,6 +12,7 @@ class BookAPIView1(APIView):
     def get(self, request, format=None):
         APIKey = self.request.query_params.get("apikey", 0)
         developer = UserProfile.objects.filter(APIkey=APIKey).first()
+        # APIKey = request.GET['apikey']  # 同上
         if developer:
             balance = developer.money
             if balance > 0:
@@ -31,10 +32,11 @@ class BookAPIView1(APIView):
 class BookAPIView2(APIView):
     """
         使用ModelSerializer
-        """
+    """
     def get(self, request, format=None):
         APIKey = self.request.query_params.get("apikey", 0)
         developer = UserProfile.objects.filter(APIkey=APIKey).first()
+        # APIKey = request.GET['apikey']  # 同上
         if developer:
             balance = developer.money
             if balance > 0:
@@ -48,3 +50,101 @@ class BookAPIView2(APIView):
                 return Response("兄弟，又到了需要充钱的时候！好开心啊！")
         else:
             return Response("查无此人啊")
+
+
+from rest_framework import mixins
+from rest_framework import generics
+
+
+class BookMixinView1(mixins.ListModelMixin, generics.GenericAPIView):
+    # 使用mixins.ListModelMixin + generics.GenericAPIView对APIView进行一次封装，至少需要加一个get函数：
+    queryset = Book.objects.all()
+    serializer_class = BookModelSerializer
+
+    def get(self, request, *args, **kwargs):       # 如果这里不加get函数，代表默认不支持get访问这个api，所以必须加上
+        APIKey = self.request.query_params.get("apikey", 0)
+        # APIKey = request.GET['apikey']  # 同上
+        developer = UserProfile.objects.filter(APIkey=APIKey).first()
+        if developer:
+            balance = developer.money
+            if balance > 0:
+                isbn = self.request.query_params.get("isbn", 0)
+                developer.money -= 1
+                developer.save()
+                self.queryset = Book.objects.filter(isbn=int(isbn))
+                return self.list(request, *args, **kwargs)
+            else:
+                return Response("兄弟，又到了需要充钱的时候！好开心啊！")
+        else:
+            return Response("查无此人啊")
+
+
+class BookMixinView2(generics.ListAPIView):
+    # 使用generics.ListAPIView则可以不用加get这个函数，
+    # 因为generics.ListAPIView相对于mixins.ListModelMixin+generics.GenericAPIView而言，
+    # 所谓的封装，就是封装了一个get函数罢了。
+    queryset = Book.objects.all()
+    serializer_class = BookModelSerializer
+
+    def get(self, request, *args, **kwargs):
+        APIKey = self.request.query_params.get("apikey", 0)
+        developer = UserProfile.objects.filter(APIkey=APIKey).first()
+
+        if developer:
+            balance = developer.money
+            if balance > 0:
+                isbn = self.request.query_params.get("isbn", 0)
+                developer.money -= 1
+                developer.save()
+                self.queryset = Book.objects.filter(isbn=int(isbn))
+                return self.list(request, *args, **kwargs)
+            else:
+                return Response("兄弟，又到了需要充钱的时候！好开心啊！")
+        else:
+            return Response("查无此人啊")
+
+
+from rest_framework import viewsets
+from rest_framework.permissions import BasePermission
+
+
+class IsDeveloper(BasePermission):
+    message = '查无此人啊'
+
+    def has_permission(self, request, view):
+        APIKey = request.query_params.get("apikey", 0)
+        developer = UserProfile.objects.filter(APIkey=APIKey).first()
+        if developer:
+            return True
+        else:
+            print(self.message)
+            return False
+
+
+class EnoughMoney(BasePermission):
+    message = "兄弟，又到了需要充钱的时候！好开心啊！"
+
+    def has_permission(self, request, view):
+        APIKey = request.query_params.get("apikey", 0)
+        developer = UserProfile.objects.filter(APIkey=APIKey).first()
+        balance = developer.money
+        if balance > 0:
+            developer.money -= 1
+            developer.save()
+            return True
+        else:
+            print(self.message)
+            return False
+
+
+class BookModelViewSet(viewsets.ModelViewSet):
+    authentication_classes = []
+    permission_classes = [IsDeveloper, EnoughMoney]
+    queryset = Book.objects.all()
+    serializer_class = BookModelSerializer
+
+    def get_queryset(self):
+        isbn = self.request.query_params.get("isbn", 0)
+        books = Book.objects.filter(isbn=int(isbn))
+        queryset = books
+        return queryset
